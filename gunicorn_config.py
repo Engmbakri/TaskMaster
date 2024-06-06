5051ab16938cda847ecadcfa60fe30512299680d
Emgmbakri <mrm507415@gmail.com>
2024-06-06 22:49:06 +0400

    Add Gunicorn configuration and update render.yaml

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 workers = 1  # Use a single worker to limit memory usage
 worker_class = 'sync'
 timeout = 120  # Increase the timeout to 120 seconds to handle long-running requests
