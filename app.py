5baeba8da02287a149a7fc267340d97a9aacebe5
Emgmbakri <mrm507415@gmail.com>
2024-06-07 18:31:34 +0400

    port verifing

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ def delete_task(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    port = int(os.environ.get('PORT', 1000))  # Use PORT from environment or default to 5000
     app.run(host='0.0.0.0', port=port)
