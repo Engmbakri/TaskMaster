f571ee39adfd0c6d371420ce575a7bef603c4c73
Emgmbakri <mrm507415@gmail.com>
2024-06-07 19:16:28 +0400

    updating

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,5 +1,9 @@
 import multiprocessing
+import os
 
 workers = 1  # Use a single worker to limit memory usage
 worker_class = 'sync'
 timeout = 120  # Increase the timeout to 120 seconds to handle long-running requests
+
+# Bind to the port specified in the PORT environment variable
+bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
