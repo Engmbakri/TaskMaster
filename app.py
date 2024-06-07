5b55ce0a84d06ff79ec610866c791c6f42fdefa1
Emgmbakri <mrm507415@gmail.com>
2024-06-07 18:22:16 +0400

    updating

--- app.py
+++ app.py
@@ -159,4 +159,5 @@ def delete_task(task_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT from environment or default to 5000
+    app.run(host='0.0.0.0', port=port)
